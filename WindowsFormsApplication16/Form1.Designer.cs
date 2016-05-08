78805d8a24665eb7d71b076151ccc55642e987f3
Казус <kir.kondraschov@yandex.ru>
2016-05-08 10:33:56 +0300

    1st Way

---
+++ WindowsFormsApplication16/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace WindowsFormsApplication16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.begin = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.begin2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.begin3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(3, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(223, 202);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.begin);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(215, 176);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Число Фибоначи";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // begin
+            // 
+            this.begin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.begin.Location = new System.Drawing.Point(16, 4);
+            this.begin.Name = "begin";
+            this.begin.Size = new System.Drawing.Size(120, 34);
+            this.begin.TabIndex = 5;
+            this.begin.Text = "НАЧАТЬ";
+            this.begin.UseVisualStyleBackColor = true;
+            this.begin.Click += new System.EventHandler(this.begin_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 59);
+            this.textBox1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(17, 82);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(174, 23);
+            this.progressBar1.Step = 50;
+            this.progressBar1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(17, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(174, 35);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Напишите число:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.Controls.Add(this.begin2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.numericUpDown1);
+            this.tabPage2.Controls.Add(this.progressBar2);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(215, 176);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Факториал";
+            // 
+            // begin2
+            // 
+            this.begin2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.begin2.Location = new System.Drawing.Point(16, 4);
+            this.begin2.Name = "begin2";
+            this.begin2.Size = new System.Drawing.Size(120, 34);
+            this.begin2.TabIndex = 9;
+            this.begin2.Text = "НАЧАТЬ";
+            this.begin2.UseVisualStyleBackColor = true;
+            this.begin2.Click += new System.EventHandler(this.begin2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Напишите число:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(17, 59);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(174, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Leave += new System.EventHandler(this.numericUpDown1_Leave);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(17, 82);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(174, 23);
+            this.progressBar2.Step = 50;
+            this.progressBar2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(17, 109);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(174, 35);
+            this.textBox3.TabIndex = 5;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.White;
+            this.tabPage3.Controls.Add(this.numericUpDown3);
+            this.tabPage3.Controls.Add(this.begin3);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.numericUpDown2);
+            this.tabPage3.Controls.Add(this.progressBar3);
+            this.tabPage3.Controls.Add(this.textBox4);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(215, 176);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Степень";
+            // 
+            // begin3
+            // 
+            this.begin3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.begin3.Location = new System.Drawing.Point(16, 4);
+            this.begin3.Name = "begin3";
+            this.begin3.Size = new System.Drawing.Size(120, 34);
+            this.begin3.TabIndex = 9;
+            this.begin3.Text = "НАЧАТЬ";
+            this.begin3.UseVisualStyleBackColor = true;
+            this.begin3.Click += new System.EventHandler(this.begin3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Напишите число и степень:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(17, 59);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(17, 82);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(174, 23);
+            this.progressBar3.Step = 50;
+            this.progressBar3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(17, 109);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(174, 35);
+            this.textBox4.TabIndex = 5;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(104, 59);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 223);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button begin;
+        private System.Windows.Forms.NumericUpDown textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button begin2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button begin3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ WindowsFormsApplication16/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //backgroundWorker1.WorkerReportsProgress = true;
+        }
+        int procent;
+        int maxValue = 0;
+        public int Fibonacci_Count(int counter, int last, int i)
+        {
+           procent = Convert.ToInt32((double)(((i - 1) - maxValue) * -1) / (double)maxValue * 100);
+           this.Invoke(new ThreadStart(delegate { progressBar1.Value = procent; }));
+            if (i == 0)
+                return counter;
+            else if( i == 1)
+                return counter;
+            else
+                return Fibonacci_Count(counter + last, counter, i - 1);
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+        }
+
+        private void begin_Click(object sender, EventArgs e)
+        {
+            //Thread inprocess = new Thread(progressChange);
+            maxValue = (int)textBox1.Value;
+            begin.Enabled = false;
+            textBox1.Enabled = false;
+            //inprocess.Start();
+            textBox2.Text = Convert.ToString(Fibonacci_Count(1, 0, maxValue));
+            maxValue = 0;
+            begin.Enabled = true;
+            textBox1.Enabled = true;
+        }
+
+        void progressChange()
+        {
+            progressBar1.Value = procent;
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void begin2_Click(object sender, EventArgs e)
+        {
+            maxValue = (int)numericUpDown1.Value;
+            begin2.Enabled = false;
+            numericUpDown1.Enabled = false;
+            //inprocess.Start();
+            textBox3.Text = Convert.ToString(Factorial_Count(1, maxValue));
+            maxValue = 0;
+            begin2.Enabled = true;
+            numericUpDown1.Enabled = true;
+        }
+
+        public int Factorial_Count(int counter, int i)
+        {
+            procent = Convert.ToInt32((double)(((i - 1) - maxValue) * -1) / (double)maxValue * 100);
+            this.Invoke(new ThreadStart(delegate { progressBar2.Value = procent; }));
+            if (i == 1)
+                return counter;
+            else
+                return Factorial_Count(counter * i, i - 1);
+        }
+
+        private void begin3_Click(object sender, EventArgs e)
+        {
+            maxValue = (int)numericUpDown3.Value;
+            begin3.Enabled = false;
+            numericUpDown2.Enabled = false;
+            //inprocess.Start();
+            textBox4.Text = Convert.ToString(Sqrt_Count((int)numericUpDown2.Value, maxValue));
+            maxValue = 0;
+            begin3.Enabled = true;
+            numericUpDown2.Enabled = true;
+        }
+        public int Sqrt_Count(int counter, int i)
+        {
+            procent = Convert.ToInt32((double)(((i - 1) - maxValue) * -1) / (double)maxValue * 100);
+            this.Invoke(new ThreadStart(delegate { progressBar3.Value = procent; }));
+            if (i == 1)
+                return counter;
+            else
+                return Sqrt_Count(counter * (int)numericUpDown2.Value, i - 1);
+        }
+    }
+}
